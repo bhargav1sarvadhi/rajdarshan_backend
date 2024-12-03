189115b581e0d23fb12bcd8a5637fa5b9ea7ad1d
bhargav1sarvadhi <135405402+bhargav1sarvadhi@users.noreply.github.com>
2024-12-03 19:27:29 +0530

    initial commit

--- src/model/hotel.model.ts
+++ src/model/hotel.model.ts
@@ -15,6 +15,30 @@ export const HotelModel = (sequelize) => {
                 type: DataTypes.TEXT,
                 allowNull: false,
             },
+            date: {
+                type: DataTypes.DATEONLY,
+                allowNull: false,
+            },
+            meal_plan: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            rate: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            extra_adult_with_mattress: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            extra_child_with_mattress: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            extra_child_without_mattress: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
             is_active: {
                 type: DataTypes.BOOLEAN,
                 defaultValue: true,
